50b8701081e394edefa4b19af7138e89b3112fb6
Pavel Safonov <p.n.safonov@gmail.com>
2024-08-29 10:08:03 +0300

    fixes

--- bin/inact/src/main.rs
+++ bin/inact/src/main.rs
@@ -9,14 +9,12 @@ fn main() {
 }
 
 fn do_main() {
-    env_logger::init();
+    env_logger::Builder::from_env(
+        env_logger::Env::default().default_filter_or("info"))
+        .init();
 
     let args: Vec<_> = env::args().collect();
     let l0 = args.len();
-    if l0 > 1 {
-        info!("The first argument is {}", args[1]);
-    }
-
     let mut days0 = DEF_DAYS.to_string();
     let mut mins0 = String::from("0");
     for (i, arg) in args.iter().enumerate() {
@@ -32,6 +30,14 @@ fn do_main() {
                 if i0 < l0 {
                     mins0 = args[i0].clone();
                 }
+            }
+            "-h"|"--help" => {
+                print_help();
+                exit(0);
+            }
+            "-v"|"--version" => {
+                print_version();
+                exit(0);
             }
             _ => (),
         }
@@ -62,6 +68,22 @@ fn do_main() {
     do_job(&ctx);
 
     info!("all is fine");
+}
+
+fn print_help() {
+    print!("Usage: inact [OPTIONS]
+inact checks last logins and do shutdown if no recent logins
+
+    -d, --days <days>     days count before current day to check for logins, default 7
+    -m, --mins <mins>     mins count before current time to check for logins
+
+    -h, --help            display this help and exit
+    -v, --version         output version information and exit`
+    ");
+}
+
+fn print_version() {
+    print!("inact  1.0.0\n");
 }
 
 struct Context {
