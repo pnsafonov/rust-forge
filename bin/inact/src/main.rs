98ae6813ca917fbd2cc9f86985fd1573a54135e8
Pavel Safonov <p.n.safonov@gmail.com>
2024-08-31 10:38:33 +0300

    fix help msg

--- bin/inact/src/main.rs
+++ bin/inact/src/main.rs
@@ -85,7 +85,7 @@ inact checks last logins and do shutdown if no recent logins
 
     -h, --help            display this help and exit
     -v, --version         output version information and exit
-`");
+");
 }
 
 fn print_version() {
